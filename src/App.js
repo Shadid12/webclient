7443d6ea8a3a9ff450ae6b6cb9a2d72b9045706c
shadid12 <shadidhaque2012@gmail.com>
2017-10-30 15:47:09 -0400

    ready to connect to api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Home from './components/Home'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello worlds</h1>
+        <Home />
       </div>
     );
   }
