4674220128e0c1e0faa912f978a8085c932361f9
shadid12 <shadidhaque2012@gmail.com>
2017-10-30 22:54:31 -0400

    added all functionalities

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import {CSVLink, CSVDownload} from 'react-csv';
 
 export default class Home extends Component {
 
 	constructor(props) {
 		super(props);
-		
+		this.state = {
+			data: [],
+			csvData: [
+				['username', 'address', 'postal' ]
+			]
+		}
+	}
+
+	componentDidMount() {
+		this._callApi();
+	}
+
+	_callApi() {
+		axios.get('http://shadid12.herokuapp.com/plain')
+		.then((res) => {
+			// this.setState({data: res.data});
+			for (var value of res.data){
+				var arr = [];
+				arr.push(value.username);
+				arr.push(value.address);
+				arr.push(value.postal);
+				this.setState({ csvData: [...this.state.csvData, arr] });
+			}
+		});
+	}
+
+	handleClick() {
+		console.log("yayay");
 	}
 
 	render() {
 		return (
-			<h3>Hello worlds</h3>
+			<button>
+				<CSVLink data={this.state.csvData}>Download Data</CSVLink>
+			</button>
 		)
 	}
 }
